f271e47118b6ff38ae322bc9228946c65102f6fb
Matthew Brown <matthew.brown.js@gmail.com>
2018-11-30 10:36:10 -0600

    config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   siteTitleAlt: "Matthew Brown's Software Blog", // Alternative site title for SEO.
   siteLogo:
     "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
-  siteUrl: "http://mbrowndev.com", // Domain of your website without pathPrefix.
+  siteUrl: "https://matthew-brown-blog.netlify.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
     "A blog about software and related technology topics.", // Website description used for RSS feeds/meta description tag.
